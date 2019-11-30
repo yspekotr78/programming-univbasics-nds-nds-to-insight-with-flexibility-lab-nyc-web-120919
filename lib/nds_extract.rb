9f5643ffe606e53534a4932321bf8163d3df66f1
Yury Spektor <yspektor78@gmail.com>
2019-11-30 13:28:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -79,7 +79,8 @@ def gross_per_studio(collection)
     index += 1
   end
 
-  p result
+  result
+
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
